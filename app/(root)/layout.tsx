f2d2d7c8efdf63f61fff178425a0ef7155b6a649
Clancy Mendonca <clancymendonca@gmail.com>
2024-07-08 17:18:55 +0530

    Plaid Banking Functionality & Dwolla Environment

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -15,17 +15,11 @@ export default async function RootLayout({
 
   return (
     <main className="flex h-screen w-full font-inter">
-      <Sidebar user={loggedIn}/>
-      
+      <Sidebar user={loggedIn} />
 
       <div className="flex size-full flex-col">
         <div className="root-layout">
-          <Image 
-            src="/icons/logo.svg"
-            width={30}
-            height={30}
-            alt="logo"
-          />
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
           <div>
             <MobileNav user={loggedIn} />
           </div>
